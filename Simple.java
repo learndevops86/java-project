d7c644adccb1b1065c0791a09886b1ad071b36de
bhasker-manikyala <28338138+bhasker-manikyala@users.noreply.github.com>
2024-03-01 22:09:02 +0530

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -1,5 +1,5 @@
 class Simple {
       public static void main(String args[]){
-	      System.out.println("Welcome to Edureka Online Training!");
+	      System.out.println("Welcome to Git's Online Training!");
       }
 }
